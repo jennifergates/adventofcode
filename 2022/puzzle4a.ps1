f03d0236a72345c5af4e37c3ffdf60ab9c83586d
jennifergates <jennifergates@users.noreply.github.com>
2022-12-05 18:04:23 -0500

    Day 4

---
+++ 2022/puzzle4a.ps1
@@ -0,0 +1,31 @@
+<#
+
+#>
+$i = get-content .\input4.txt -encoding utf8
+
+$sum = 0
+
+foreach ($pair in $i) {
+	
+	$r1, $r2 = $pair.split(',')
+	
+	$r1begin = [int]$r1.split('-')[0]
+	$r1end = [int]$r1.split('-')[1]
+	$r2begin = [int]$r2.split('-')[0]
+	$r2end = [int]$r2.split('-')[1]
+	
+<# 	write-host $pair
+	write-host $r1begin
+	write-host $r1end
+	write-host $r2begin
+	write-host $r2end #>
+	
+	if ( ($r1begin -ge $r2begin -and $r1end -le $r2end) -or ( $r2begin -ge $r1begin -and $r2end -le $r1end) ) {
+		write-host "$pair within"
+		$sum = $sum +1
+	}
+}
+
+write-host $sum
+
+
