4a9d082e93de77bf6a5764387d2ac27b42100d05
jennifergates <jennifergates@users.noreply.github.com>
2022-12-07 20:17:31 -0500

    Day 6

---
+++ 2022/puzzle6b.ps1
@@ -0,0 +1,28 @@
+$i = get-content .\input6.txt -encoding utf8
+
+for ($startwin = 0; $startwin -lt $i.length -13; $startwin = $startwin +1){
+	$slice = $i.substring($startwin,14)
+	write-host "`nnew slice: $slice"
+	
+	$duplicated = "False"
+	foreach ($x in $slice.tochararray()){
+		
+		#write-host $slice.indexof($x)
+		#write-host $slice.lastindexof($x)
+		if ($slice.indexof($x) -ne $slice.lastindexof($x)){
+			$duplicated = "True"
+			write-host "$x is duplicated in $slice"
+		} else {
+			write-host "$x is not duplicated in $slice"
+		}
+	}
+	
+	if ($duplicated -eq "False") {
+		write-host "first non-duplicate slice $slice ends at $startwin plus 14?" 
+		break
+	} else {
+		write-host "there is a duplicate in $slice, continuing"
+	}
+	
+	
+}
